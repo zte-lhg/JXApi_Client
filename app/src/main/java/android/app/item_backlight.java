132082399d11daed2060de3e4f0414152d51a3dd
quanjunlin <junlin.quan@qq.com>
2025-09-01 23:25:19 +0800

    dos2unix for system app file

--- app/src/main/java/android/app/item_backlight.java
+++ app/src/main/java/android/app/item_backlight.java
@@ -3,15 +3,16 @@ package android.app;
 import android.app.util.AIDLUtil;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import jxapi.SysControl;
 
 public class item_backlight extends Activity implements View.OnClickListener {
     private static final String TAG = "item_backlight";
+    private SysControl jx_sysControl;
     private SeekBar seekbar_backlight;
     private TextView tv_brightness;
     private Button btn_openBacklight;
@@ -22,10 +23,7 @@ public class item_backlight extends Activity implements View.OnClickListener {
         requestWindowFeature(1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_backlight);
-
-        //绑定api服务
-       AIDLUtil.getInstance().bindService(item_backlight.this);
-
+        jx_sysControl = SysControl.GetInstance();
         this.seekbar_backlight = (SeekBar) findViewById(R.id.seekbar_backlight);
 
         btn_openBacklight=(Button) findViewById(R.id.btn_openBacklight);
@@ -35,14 +33,11 @@ public class item_backlight extends Activity implements View.OnClickListener {
 
         tv_brightness = findViewById(R.id.tv_progress);
 
-        //初始化
-        //需要延时一下，绑定api服务需要时间
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 // 延迟执行的操作
                 try {
-
                    int progress = AIDLUtil.getInstance().ax_getBacklight();
                     Log.i(TAG, "progress="+progress);
                     seekbar_backlight.setProgress(progress);
@@ -55,7 +50,6 @@ public class item_backlight extends Activity implements View.OnClickListener {
             }
         }, 400); // 延迟
 
-
         seekbar_backlight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
@@ -67,15 +61,11 @@ public class item_backlight extends Activity implements View.OnClickListener {
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
                 switch (seekBar.getId()) {
                     case R.id.seekbar_backlight:
-
                         try {
                             Log.i(TAG, "设置背光s");
-
-                            AIDLUtil.getInstance().ax_setBacklight(progress);
-
+                            jx_sysControl.setBrightness(progress);
                             Log.i(TAG, "pic.setBacklight()=" + progress);
                             TextView textView = tv_brightness;
                             textView.setText("" + String.valueOf(progress));
@@ -94,22 +84,22 @@ public class item_backlight extends Activity implements View.OnClickListener {
             }
         });
     }
+    
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_closeBacklight:
                 try {
                     Log.i(TAG, "关闭背光");
-                    AIDLUtil.getInstance().ax_EnableBacklight(false);
+                    jx_sysControl.setLcdBackLight(0);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-
                 break;
             case  R.id.btn_openBacklight:
                 try {
                     Log.i(TAG, "打开背光");
-                    AIDLUtil.getInstance().ax_EnableBacklight(true);
+                    jx_sysControl.setLcdBackLight(1);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
