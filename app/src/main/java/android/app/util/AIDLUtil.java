08d69a8384588d032302bcd77f14b167f123faed
quanjunlin <junlin.quan@qq.com>
2025-09-01 21:37:05 +0800

    add new jar and api lib

--- app/src/main/java/android/app/util/AIDLUtil.java
+++ app/src/main/java/android/app/util/AIDLUtil.java
@@ -8,13 +8,13 @@ import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
 
-import com.app.sdk_api_service.IAidlInterface;
+//import com.app.sdk_api_service.IAidlInterface;
 
 import androidx.annotation.NonNull;
 
 public class AIDLUtil {
     private static AIDLUtil aidlUtil;
-    private IAidlInterface iService;
+//    private IAidlInterface iService;
 
     private AIDLUtil() {
     }
@@ -30,18 +30,18 @@ public class AIDLUtil {
         return aidlUtil;
     }
 
-    private ServiceConnection connection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            Log.i("LEO", "绑定成功");
-            iService = IAidlInterface.Stub.asInterface(service);
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            iService = null;
-        }
-    };
+//    private ServiceConnection connection = new ServiceConnection() {
+//        @Override
+//        public void onServiceConnected(ComponentName name, IBinder service) {
+//            Log.i("LEO", "绑定成功");
+////            iService = IAidlInterface.Stub.asInterface(service);
+//        }
+//
+//        @Override
+////        public void onServiceDisconnected(ComponentName name) {
+////            iService = null;
+////        }
+//    };
 
     public void bindService(Context context) {
         Intent intent = new Intent("com.app.sdk_api_service");
@@ -49,16 +49,16 @@ public class AIDLUtil {
        // Intent intent = new Intent("com.leo.aidl");
        // intent.setPackage("com.leo.aidl");
 
-        context.bindService(intent, connection, Context.BIND_AUTO_CREATE);
+//        context.bindService(intent, connection, Context.BIND_AUTO_CREATE);
     }
 
     public void unbindService(Context context) {
-        if (null == iService) {
-            return;
-        }
-        Log.i("LEO", "取消绑定");
-        context.unbindService(connection);
-        iService = null;
+//        if (null == iService) {
+//            return;
+//        }
+//        Log.i("LEO", "取消绑定");
+//        context.unbindService(connection);
+//        iService = null;
     }
 
     /*
@@ -81,55 +81,55 @@ public class AIDLUtil {
      */
 
     public Integer ax_getBacklight() {
-        if (null == iService) {
-            Log.i("LEO", "iService为null");
-            return null;
-        }
-        try {
-            int data = iService.ax_getBacklight();
-            return data;
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
+//        if (null == iService) {
+//            Log.i("LEO", "iService为null");
+//            return null;
+//        }
+//        try {
+//            int data = iService.ax_getBacklight();
+//            return data;
+//        } catch (RemoteException e) {
+//            e.printStackTrace();
+//        }
         return null;
     }
     public void ax_setBacklight(int data) {
-        if (null == iService) {
-            Log.i("LEO", "iService为null");
-            return ;
-        }
-        try {
-           iService.ax_setBacklight(data);
-
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
+//        if (null == iService) {
+//            Log.i("LEO", "iService为null");
+//            return ;
+//        }
+//        try {
+//           iService.ax_setBacklight(data);
+//
+//        } catch (RemoteException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public void ax_EnableUart(Boolean flag) {
-        if (null == iService) {
-            Log.i("LEO", "iService为null");
-            return ;
-        }
-        try {
-            iService.ax_EnableUart(flag);
-
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
+//        if (null == iService) {
+//            Log.i("LEO", "iService为null");
+//            return ;
+//        }
+//        try {
+//            iService.ax_EnableUart(flag);
+//
+//        } catch (RemoteException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public void ax_EnableBacklight(Boolean flag) {
-        if (null == iService) {
-            Log.i("LEO", "iService为null");
-            return ;
-        }
-        try {
-            iService.ax_EnableBacklight(flag);
-
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
+//        if (null == iService) {
+//            Log.i("LEO", "iService为null");
+//            return ;
+//        }
+//        try {
+//            iService.ax_EnableBacklight(flag);
+//
+//        } catch (RemoteException e) {
+//            e.printStackTrace();
+//        }
     }
 /*
     public int ax_getCurSourceId(int var1) {
